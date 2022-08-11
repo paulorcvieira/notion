616a5dd2d29552dda740832d429cc4c1d45fb534
Paulo Vieira <paullocostta86@gmail.com>
2022-08-11 09:50:24 -0300

    feat: Created default layout

---
+++ src/components/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import { ReactNode } from 'react'
+
+import { Header, LogoVariant } from 'components/Header'
+import { ColorVariant } from 'components/Header/styles'
+
+import { Container } from './styles'
+
+interface DefaultLayoutProps {
+  title: string
+  description: string
+  children: ReactNode
+  logoFileName: string
+  headerTitle: string
+  headerLogo?: LogoVariant
+  headerColor?: ColorVariant
+  headerLogoAlt?: string
+}
+
+export default function DefaultLayout({
+  title,
+  description,
+  children,
+  logoFileName,
+  headerTitle,
+  headerLogo,
+  headerColor,
+  headerLogoAlt,
+}: DefaultLayoutProps) {
+  return (
+    <Container>
+      <Head>
+        <link rel="shortcut icon" href={`/assets/${logoFileName}`} />
+        <link rel="apple-touch-icon" href={`/assets/${logoFileName}`} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href={`/assets/${logoFileName}`} />
+      </Head>
+
+      <Header
+        title={headerTitle}
+        logo={headerLogo}
+        color={headerColor}
+        logoAlt={headerLogoAlt}
+      />
+
+      {children}
+    </Container>
+  )
+}
